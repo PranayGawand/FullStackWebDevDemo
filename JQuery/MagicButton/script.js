e65810ca61f8c31dbaade13d6bddbca1a30d6697
jiyange <labixiaoyangji@qq.com>
2015-11-17 11:50:37 -0700

    update

--- JQuery/MagicButton/script.js
+++ JQuery/MagicButton/script.js
@@ -1,11 +1,12 @@
 $(function(){
-    $(document).ready(function() {
-        $('div').mouseenter(function() {
-            $('div').fadeTo('fast',1);
-        });
-        $('div').mouseleave(function() {
-            $('div').fadeTo('fast',0.5);
-        });
+
+   // jQuery methods go here...
+    $('div').mouseenter(function() {
+         $('div').fadeTo('fast',1);
     });
-    
-}
+    $('div').mouseleave(function() {
+        $('div').fadeTo('fast',0.5);
+    });
+
+});
+
