5c81edd4988b399dbd75e4eee5d834a45636bfb6
jiyange <labixiaoyangji@qq.com>
2015-11-19 12:49:01 -0700

    Use jQuery to monitor a form

--- JQuery/New Folder/app.js
+++ JQuery/New Folder/app.js
@@ -1,37 +1,37 @@
 var $main = function() {
   $('form').submit(function() {
-    var $firstName = $('#first').text();
-    var $lastName = $('#last').text();;
-    var $email = $('#email').text();;
-    var $password = $('#password').text();;
+    var $firstName = $('#first').val();
+    var $lastName = $('#last').val();
+    var $email = $('#email').val();
+    var $password = $('#password').val();
     var $flag = true;
     if($firstName === "") {
-      $(".first-name-error").val("Please enter your first name");
+      $(".first-name-error").text("Please enter your first name");
       $flag = false;
       console.log(1);
     }
     
     if($lastName === "") {
-      $(".last-name-error").val("Please enter your last name");
+      $(".last-name-error").text("Please enter your last name");
       $flag = false;
     }
     
     if($email === "") {
-      $(".email-error").val("Please enter your email address");
+      $(".email-error").text("Please enter your email address");
       $flag = false;
     } else if ($email === "jyang7@ualberta.ca") {
-      $(".email-error").val("This email is already taken");
+      $(".email-error").text("This email is already taken");
       $flag = false;
     }
     
     if($password === "") {
-      $(".password-error").val("Please enter a password");
+      $(".password-error").text("Please enter a password");
       $flag = false;
     } else if ($password.length <= 7) {
-      $(".password-error").val("Short passwords are easy to guess");
+      $(".password-error").text("Short passwords are easy to guess");
       $flag = false;
     }
-    return true;
+    return $flag;
   })
 }
 
