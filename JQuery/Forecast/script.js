6db79dd345f91d3253a8eb34327e32a8964cbc34
jiyange <labixiaoyangji@qq.com>
2015-11-20 07:19:01 -0700

    Forecast

---
+++ JQuery/Forecast/script.js
@@ -0,0 +1,14 @@
+var main = function() {
+  $('.day').click(function(){
+  	$(this).next().toggle('slow');
+    $(this).find('.weekday span').toggleClass('glyphicon-minus'); 
+  });
+};
+
+$(document).ready(main);
+
+
+
+
+$("#td_id").attr('class', 'newClass');
+
