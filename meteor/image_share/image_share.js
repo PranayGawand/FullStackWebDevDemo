e3776f3b57e8e03261c790c02ae26aead57af3d3
Ji Yang (杨佶) <labixiaoyangji@qq.com>
2015-12-12 06:16:07 -0700

    Include a bootstrap thumbnail into template

--- meteor/image_share/image_share.js
+++ meteor/image_share/image_share.js
@@ -1,11 +1,21 @@
 if (Meteor.isClient) {
 
     // console.log("I am the client");
-    var img_data = {
+    var img_data = [
+        {
         img_src:"lion.jpg",
         img_alt:"A lion"
-    };
-    Template.images.helpers(img_data);
+        },
+        {
+        img_src:"baseball.jpg",
+        img_alt:"baseball"
+        },
+        {
+        img_src:"baseball2.jpg",
+        img_alt:"baseball2"
+        }
+    ];
+    Template.images.helpers({images:img_data});
 }
 
 if (Meteor.isServer) {
