6831184663cae3f5485c00fc08b69edcdfe2384f
jiyange <program.ji.yang@gmail.com>
2015-12-21 01:00:49 -0700

    Add some comments to Angular conFusion project

--- Coursera_AngularJS/conFusion/app/scripts/app.js
+++ Coursera_AngularJS/conFusion/app/scripts/app.js
@@ -2,12 +2,15 @@
 
 angular.module('confusionApp', [])
 
+    
+        // menu page controller
         .controller('MenuController', ['$scope', function($scope) {
             
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-
+            
+            // dishes data 
             $scope.dishes=[
                          {
                           name:'Uthapizza',
@@ -46,7 +49,8 @@ angular.module('confusionApp', [])
                            comment: ''
                         }
                         ];
-                        
+            
+            // tab manipulation             
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
                 
@@ -64,36 +68,46 @@ angular.module('confusionApp', [])
                 }
             };
 
+            // check whether selected tab is active, return true is selected
             $scope.isSelected = function (checkTab) {
                 return ($scope.tab === checkTab);
             };
     
+            
             $scope.toggleDetails = function() {
                 $scope.showDetails = !$scope.showDetails;
             };
         }])
 
+        
+        // contactus page controller 
         .controller('ContactController', ['$scope', function($scope) {
 
+            // a feedback object
             $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
             
+            // question: ? why not $scope
             var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
             
             $scope.channels = channels;
             $scope.invalidChannelSelection = false;
                         
         }])
 
+        // feedback form controller, at page contactus.html
         .controller('FeedbackController', ['$scope', function($scope) {
             
             $scope.sendFeedback = function() {
                 
                 console.log($scope.feedback);
                 
+                
                 if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
                     $scope.invalidChannelSelection = true;
                     console.log('incorrect');
                 }
+                
                 else {
                     $scope.invalidChannelSelection = false;
                     $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
@@ -152,7 +166,7 @@ angular.module('confusionApp', [])
 
         .controller('DishCommentController', ['$scope', function($scope) {
 
-             //Step 1: Create a JavaScript object to hold the comment from the form
+             // create a JavaScript object to hold the comment from the form
                 $scope.comment = {
                     rating: 5,
                     author: "",
@@ -162,14 +176,17 @@ angular.module('confusionApp', [])
 
                 $scope.submitComment = function () {
 
+                    // change the time format to readable format
                     $scope.comment.date = new Date().toISOString();
 
                     console.log($scope.comment);
-
+                    // add the comment object to dish.comments
                     $scope.dish.comments.push($scope.comment);
-
+                    
+                    // set the form to the original status
                     $scope.commentForm.$setPristine();
 
+                    // set the default rating to 5, others to empty
                     $scope.comment = {
                         rating: 5,
                         author: "",
