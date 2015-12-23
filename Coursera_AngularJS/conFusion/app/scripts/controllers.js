9437705c2588a2c740a7bb470f740de61b78e783
jiyange <program.ji.yang@gmail.com>
2015-12-22 19:40:11 -0700

    Change pages to a template

--- Coursera_AngularJS/conFusion/app/scripts/controllers.js
+++ Coursera_AngularJS/conFusion/app/scripts/controllers.js
@@ -81,8 +81,8 @@ angular.module('confusionApp')
         }])
 
         .controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            
             $scope.dish= menuFactory.getDish(3);
-        };
             
         }])
 
