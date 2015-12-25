b8878c9d1e0fd5d58a524de6cbc1b27b52393657
jiyange <program.ji.yang@gmail.com>
2015-12-25 16:10:37 -0700

    Clean up image_share code and setup the siteace project

--- meteor/image_share/lib/collections.js
+++ meteor/image_share/lib/collections.js
@@ -1,27 +1,37 @@
 // this is image_share.js
 Images = new Mongo.Collection("images");
 
-
+// set up security on Images collection
 Images.allow({
-    insert:function(user_id, doc) {
-        console.log("Testing insertion");
-        if (Meteor.user()) {
-            console.log(doc);
-            // force the image to be owned by the user
-            doc.createBy = user_id;
-            if (user_id != doc.createBy){
-                return false;
-            } else {
-                return true;
-            } 
-        } else {
-            return false;
-        }
-    },
-    remove:function(user_id, doc) {
-        console.log("Testing removal")
-        return true;
-    }
-    
-    
+
+	// we need to be able to update images for ratings.
+	update:function(userId, doc){
+		console.log("testing security on image update");
+		if (Meteor.user()){// they are logged in
+			return true;
+		} else {// user not logged in - do not let them update  (rate) the image. 
+			return false;
+		}
+	},
+
+	insert:function(userId, doc){
+		console.log("testing security on image insert");
+		if (Meteor.user()){// they are logged in
+			if (userId != doc.createdBy){// the user is messing about
+				return false;
+			}
+			else {// the user is logged in, the image has the correct user id
+				return true;
+			}
+		}
+		else {// user not logged in
+			return false;
+		}
+	}, 
+	remove:function(userId, doc){
+		return true;
+	}
 })
+
+
+
